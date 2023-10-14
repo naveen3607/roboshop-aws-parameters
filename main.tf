1e4a850b213ac80ed8e1682bae838d1f9536536a
naveen3607 <naveenreddykallam3607@gmail.com>
2023-10-14 05:30:22 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "aws_ssm_parameter" "main" {
   name  = each.key
   type  = each.value["type"]
   value = each.value["value"]
+  key_id = "arn:aws:kms:us-east-1:207153251982:key/91ab5fca-9ae7-4c4e-9dd8-f1e1ee3da95f"
 }
